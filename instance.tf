53c2563c7a6c45bd4ab581cbe68b213b03fc01a6
Sirin Ali <118505493+sirinali07@users.noreply.github.com>
2023-12-28 22:11:39 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -4,7 +4,8 @@
   secret_key=var.AWS_SECRET_KEY 
   } 
 
-  resource "aws_instance" "web" { 
+  resource "aws_instance" "web" {
+  count = 2
   ami           = "ami-0fc5d935ebf8bc3bc" 
   instance_type = "t3.micro" 
   tags = { 
